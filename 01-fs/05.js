8566ccce06c1e1f35b49b4ec5938771ecdd3b16f
guobing1993 <qq.1131788816@live.com>
2017-12-01 09:49:06 +0800

    modify

--- 01-fs/05.js
+++ 01-fs/05.js
@@ -1,5 +1,5 @@
 const mkdirs = require('./mkdir.js');
-mkdirs('demo\\demo', err => {
+mkdirs('demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo\\demo', err => {
     if (err) {
         console.log(err)
 
--- 01-fs/mkdir.js
+++ 01-fs/mkdir.js
@@ -2,17 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 function mkdirs(pathname, callback) {
+    let root = path.dirname(module.parent.filename)
 
-    pathname = path.isAbsolute(pathname) ? pathname : path.join(__dirname, pathname);
-    let relativePath = path.relative(__dirname, pathname);
+    pathname = path.isAbsolute(pathname) ? pathname : path.join(root, pathname);
+    let relativePath = path.relative(root, pathname);
 
     let folders = relativePath.split(path.sep);
 
     try {
         let pre = '';
         folders.forEach(folder => {
+            let dir = path.join(root, pre, folder);
 
-            fs.mkdirSync(path.join(__dirname, pre, folder));
+            fs.stat(dir, (err, stats) => {
+                if (err) {
+                    fs.mkdirSync(dir);
+                }
+            })
+
             pre = path.join(pre, folder);
         });
         callback && callback(null);
