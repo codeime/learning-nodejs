b24dcab8c2471d68b8ee8bb90cce0f2e8a5404a8
guobing1993 <qq.1131788816@live.com>
2017-12-19 17:13:59 +0800

    add

--- 07-myBlog/routes/signin.js
+++ 07-myBlog/routes/signin.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 router.get('/', checkNotLogin, (req, res, next) => {
-    res.send('登陆页');
+    res.render('sigin');
 })
 router.post('/', checkNotLogin, (req, res, next) => {
-    res.send('登陆页');
+    const
+        res.send('登陆页');
 })
 
 
--- 07-myBlog/routes/signout.js
+++ 07-myBlog/routes/signout.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
 router.get('/', checkNotLogin, (req, res, next) => {
-    res.send('退出登陆')
+    req.session.user = null;
+    req.flash("success", '登出成功');
+    res.redirect('/posts')
 })
 
 module.exports = router;
