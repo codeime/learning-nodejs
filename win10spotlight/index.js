1abef78be63334b1051433a8427bda87105764dd
guobing1993 <qq.1131788816@live.com>
2017-12-26 17:29:26 +0800

    modify

--- win10spotlight/index.js
+++ win10spotlight/index.js
@@ -9,23 +9,35 @@ fs.mkdirSync(src);
 const files=fs.readdirSync(target);
 
 files.forEach(file=>{
+
     const stats=fs.statSync(path.join(target,file));
+    
     if(stats.isFile()){
+
       fs.readFile(path.join(target,file),(err,data)=>{
+
         const tempData=data.slice(0,7);
         let filename;
 
         if(tempData.indexOf( Buffer.from('FFD8FF',"hex"))!=-1){
-          filename=path.join(src,file+".jpg");  
-        }else if(tempData.indexOf( Buffer.from('89504E47','hex'))!=-1){
-          filename=path.join(src,file+".png");
-        }
-        if(filename){
-             fs.writeFile(filename,data,err=>{
-                if(err)console.log(err);
-             });
-        }
 
-   })  
+          filename=path.join(src,file+".jpg");  
+
+        }else if(tempData.indexOf( Buffer.from('89504E47','hex'))!=-1){
+
+          filename=path.join(src,file+".png");
+         }
+
+        if(filename){
+
+         fs.writeFile(filename,data,err=>{
+
+            if(err)console.log(err);
+
+        });
+
+     }
+
+ })  
   }
 })
