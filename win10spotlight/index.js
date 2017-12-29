7627eb37447447a482128ed63c80e7c41ce60386
guobing1993 <1131788816@qq.com>
2017-12-29 22:20:05 +0800

    }}}}}}}

--- win10spotlight/index.js
+++ win10spotlight/index.js
@@ -3,59 +3,77 @@ const path = require('path');
 const imageInfo = require('image-info');
 
 
+
 const target = path.join(process.env.localappdata, "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\");
 const src = path.join(process.env.userprofile, 'Pictures\\spotlight');
 
 try {
-  const s = fs.statSync(src);
-  if (!s.isDirectory()) {
-    fs.mkdirSync(src);
-  }
+	const s = fs.statSync(src);
+	if (!s.isDirectory()) {
+		fs.mkdirSync(src);
+	}
 } catch (error) {
-  fs.mkdirSync(src);
+	fs.mkdirSync(src);
 }
 
 const files = fs.readdirSync(target);
 
+
+
 files.forEach(file => {
 
-  const stats = fs.statSync(path.join(target, file));
+	const stats = fs.statSync(path.join(target, file));
 
-  if (stats.isFile()) {
+	if (stats.isFile()) {
 
-    fs.readFile(path.join(target, file),(err, data)=>{
+		fs.readFile(path.join(target, file),(err, data)=>{
 
-      const tempData = data.slice(0, 7);
-      let filename;
+			const tempData = data.slice(0, 7);
+			let filename;
 
-      if (tempData.indexOf(Buffer.from('FFD8FF', "hex")) != -1) {
+			if (tempData.indexOf(Buffer.from('FFD8FF', "hex")) != -1) {
 
-        filename = path.join(src, file + ".jpg");
+				filename = path.join(src, file + ".jpg");
 
-      } else if (tempData.indexOf(Buffer.from('89504E47', 'hex')) != -1) {
+			} else if (tempData.indexOf(Buffer.from('89504E47', 'hex')) != -1) {
 
-        filename = path.join(src, file + ".png");
-      }
+				filename = path.join(src, file + ".png");
+			}
 
-      if (filename) {
+			if (filename) {
 
-        fs.writeFile(filename, data, err=>{
+				fs.writeFile(filename, data, err=>{
 
-          if (err) console.log(err);
-          imageInfo(filename, (err, info) => {
-            var wh=info.height/info.width;
-            if(wh==1){
-                fs.unlink(filename,(err)=>{
-                    if(err){console.log(err)}
-                })
-            }
-          })
-        
-    });
+					if (err) console.log(err);
+					
+					imgInfo(filename)
+					.then(function(data){
+						var wh=data.height/data.width;
+						if(wh==1){
+							fs.unlink(filename,(err)=>{
+								if(err){console.log(err)}
+							})
+						}
+					})
+					.catch(function(err){
+						console.log(err);
+					})
+				});
 
-      }
+			}
 
-    })
-  }
+		})
+	}
 })
 
+function imgInfo(filename){
+	return new Promise(function(resolve,reject){
+		imageInfo(filename, (err, info) => {
+			if(err){
+				reject(err);
+			}
+			resolve(info);
+			
+		})
+	})
+}
