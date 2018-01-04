6926c72c4afc0a34afbc174857d6f5e1cab09911
guobing1993 <qq.1131788816@live.com>
2018-01-04 14:12:46 +0800

    remove console

--- littleAlbum/controller/router.js
+++ littleAlbum/controller/router.js
@@ -2,7 +2,7 @@ const file = require("../models/file.js")
 module.exports.showIndex = function (req, res, next) {
     file.getAllAlbum(function (err, data) {
         if (err) {
-            console.log(err);
+
             next();
             return;
         }
@@ -15,7 +15,6 @@ module.exports.showIndex = function (req, res, next) {
 module.exports.showAlbum = function (req, res, next) {
     if (req.params.album != "favicon.ico") {
 
-        console.log(req.params.album)
         file.getImgByAlbumName(req.params.album, function (err, data) {
             if (err) {
 
