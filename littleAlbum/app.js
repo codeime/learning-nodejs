39cbf742052949e80bb8a9ac29e244233235fadd
guobing1993 <qq.1131788816@live.com>
2018-01-08 17:07:46 +0800

    “done upload

--- littleAlbum/app.js
+++ littleAlbum/app.js
@@ -2,14 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const router = require('./controller')
+const formidable = require('express-formidable');
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "uploads")));
+app.use(formidable({
+    uploadDir: path.join(__dirname, "/temp"),
+    keepExtensions: true
+}))
+
+
 
 app.get('/', router.showIndex);
-app.get('/:album', router.showAlbum);
+app.get('/up', router.showUp);
+app.post('/up', router.upFile);
+app.get('/album/:album', router.showAlbum);
+
+
 app.use(function (req, res) {
     res.render('err')
 })
--- littleAlbum/controller/router.js
+++ littleAlbum/controller/router.js
@@ -1,4 +1,6 @@
 const file = require("../models/file.js")
+const fs = require('fs');
+const path = require('path');
 module.exports.showIndex = function (req, res, next) {
     file.getAllAlbum(function (err, data) {
         if (err) {
@@ -30,4 +32,33 @@ module.exports.showAlbum = function (req, res, next) {
             })
         })
     }
+}
+
+module.exports.showUp = function (req, res, next) {
+
+    file.getAllAlbum(function (err, data) {
+        if (err) {
+
+            next();
+            return;
+        }
+        res.render('upload', {
+            album: data
+        })
+    })
+}
+
+module.exports.upFile = function (req, res, next) {
+    const album = req.fields.album;
+    const src = req.files.file.path;
+    const filename = src.split(path.sep).pop();
+    let target = path.join(module.parent.filename, "../uploads/" + album + "/" + filename);
+    fs.rename(src, target, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.redirect('/up');
+    })
+
 }
