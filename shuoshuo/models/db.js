eada18177b735b60876adc752d053a53cc07e59c
guobing1993 <1131788816@qq.com>
2018-01-10 20:11:17 +0800

    collection not a function

--- shuoshuo/models/db.js
+++ shuoshuo/models/db.js
@@ -1,19 +1,17 @@
 const MongoClient = require('mongodb').MongoClient;
 
-function __connect(fn) {
+function __connectDB(callback) {
     var url = "mongodb://localhost:27017/shuoshuo";
+
     MongoClient.connect(url, function (err, db) {
-        if (err) {
-            fn("出错 ", null);
-            return;
-        }
-        fn(null, db);
+
+        callback(err, db);
     })
 }
 
 module.exports.insertOne = function (collectionNmae, json, callback) {
 
-    __connect(function (err, db) {
+    __connectDB(function (err, db) {
         db.collection(collectionNmae).insertOne(json, function (err, result) {
             callback(err, result);
             db.close();
@@ -30,15 +28,20 @@ module.exports.find = function (collectionName, json, c, d) {
     } else if (arguments.length == 4) {
         let callback = d;
         let args = c;
-        let skip = args.page * args.pageSize;
-        let limit = args.page;
+        let skip = args.page * args.pageSize || 0;
+        let limit = args.page || 0;
+        let sort = args.sort || {};
     } else {
         throw new Error("find参数错误");
         return;
     }
 
-    __connect(function (err, db) {
-        let cursor = db.collection(collection).find(json).skip(skip).limit(limit);
+    __connectDB(function (err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        let cursor = db.collection(collectionName).find(json).skip(skip).limit(limit).sort(sort);
         let result = [];
         cursor.each((err, doc) => {
             if (err) {
@@ -55,21 +58,21 @@ module.exports.find = function (collectionName, json, c, d) {
     })
 }
 module.exports.delete = function (collectionName, json, callback) {
-    __connect(function (err, db) {
+    __connectDB(function (err, db) {
         db.collection(collectionName).deleteMany(json, function (err, result) {
             callback(err, result);
         })
     })
 }
 module.exports.updata = function (collectionName, json, callback) {
-    __connect(function (err, db) {
+    __connectDB(function (err, db) {
         db.collection(collectionName).updataMany(json1, json2, function (err, result) {
             callback(err, result)
         })
     })
 }
 module.exports.count = function (collectionName, json, callback) {
-    __connect(function (err, db) {
+    __connectDB(function (err, db) {
         db.collection(collectionName).find(json).count((err, count) => {
             callback(err, count);
         });
--- shuoshuo/router/router.js
+++ shuoshuo/router/router.js
@@ -1,4 +1,5 @@
-const formidable = require('formidable')
+const formidable = require('formidable');
+const db = require('../models/db')
 
 
 module.exports.showIndex = function (req, res, next) {
@@ -12,7 +13,18 @@ module.exports.doRegist = function (req, res, next) {
     form.parse(req, function (err, fields, files) {
         var username = fields.username;
         var password = fields.password;
-
+        db.find("user", {
+            username: username
+        }, function (err, result) {
+            if (err) {
+                res.send(-3); /* 服务器错误 */
+                return;
+            }
+            if (result.length != 0) {
+                res.send(-1); /* 该用户名存在 */
+                return;
+            }
+        })
     })
     res.render('regist')
 }
